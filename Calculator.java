8f1de0980661bac4907819a5ccbf22894f47f75a
c-puma <cpumal@unsa.edu.pe>
2022-05-05 09:58:07 -0500

    Se crearon los metodos

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,28 @@ public class Calculator {
 
 	}
 
+	public static int add(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+	
+	public static int sub(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+	
+	public static int mul(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+	
+	public static int div(int a, int b) {
+		int result = a / b;
+		return result;
+	}
+	
+	public static int mod(int a, int b) {
+		int result = a % b;
+		return result;
+	}
 }
